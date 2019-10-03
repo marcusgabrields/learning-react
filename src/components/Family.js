8ddfb3521d639a2b15438cf515b7afd3cc09b97d
Gabriel <marcusgabriel@locus.software>
2019-10-03 15:10:01 -0300

    Propagating "props" to child components 2

--- src/components/Family.js
+++ src/components/Family.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { childrenWithProps } from '../helpers/helper';
+
 
 export default props =>
   <div>
     <h1>Family</h1>
-    { React.cloneElement(props.children, { ...props }) }
+
+    { childrenWithProps(props) }
+
+    {/* { React.Children.map(props.children, child => {
+      return React.cloneElement(child, { ...props })
+    }) } */}
+
+    {/* { React.cloneElement(props.children, { ...props }) } */}
 
     {/* { React.cloneElement(props.children,
       { last_name: props.last_name }) } */}
