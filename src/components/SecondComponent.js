5665485023df0e25d10981dc43e87c3f5be64ec2
Gabriel <marcusgabriel@locus.software>
2019-10-03 14:17:45 -0300

    Default export and normal export

--- src/components/SecondComponent.js
+++ src/components/SecondComponent.js
@@ -5,4 +5,5 @@ const CompA = props => <h1>First says: { props.value }</h1>;
 
 const CompB = props => <h1>Second says: { props.value }</h1>;
 
-export { CompA, CompB };
+export { CompB };
+export default CompA;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import FirstComponent from './components/FirstComponent';
-// import { CompA, CompB } from './components/SecondComponent';
-import MultiElements from './components/MultiElements';
+import CompA, { CompB } from './components/SecondComponent';
+// import MultiElements from './components/MultiElements';
 
 
 const element = document.getElementById('root');
 ReactDOM.render(
   <div>
-    <MultiElements />
+    {/* <MultiElements /> */}
 
-    {/* <CompA value='Hi Im A!'/>
-    <CompB value='B here!' /> */}
+    <CompA value='Hi Im A!'/>
+    <CompB value='B here!' />
 
     {/* <FirstComponent value='Good Morning!' />
     <FirstComponent value={42} /> */}
