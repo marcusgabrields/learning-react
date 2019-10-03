855223214d7957b27421130c86180180a4349bc3
Gabriel <marcusgabriel@locus.software>
2019-10-03 13:52:14 -0300

    Returning multiple elements

---
+++ src/components/MultiElements.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default props =>[
+    <h1>Part 1</h1>,
+    <h2>Part 2</h2>
+]
+
+// export default props =>
+//   <React.Fragment>
+//     <h1>Part 1</h1>
+//     <h2>Part 2</h2>
+//   </React.Fragment>
+
+// export default props =>
+//   <div>
+//     <h1>Part 1</h1>
+//     <h2>Part 2</h2>
+//   </div>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import FirstComponent from './components/FirstComponent';
-import { CompA, CompB } from './components/SecondComponent';
+// import { CompA, CompB } from './components/SecondComponent';
+import MultiElements from './components/MultiElements';
 
 
 const element = document.getElementById('root');
 ReactDOM.render(
   <div>
-    <CompA value='Hi Im A!'/>
-    <CompB value='B here!' />
+    <MultiElements />
+
+    {/* <CompA value='Hi Im A!'/>
+    <CompB value='B here!' /> */}
 
     {/* <FirstComponent value='Good Morning!' />
     <FirstComponent value={42} /> */}
