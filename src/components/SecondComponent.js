8c72e06002f38fa762e3d05a2a0f33f26edf2a8d
Gabriel <marcusgabriel@locus.software>
2019-10-03 13:34:43 -0300

    Exporting more than one component

---
+++ src/components/SecondComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const CompA = props => <h1>First says: { props.value }</h1>;
+
+const CompB = props => <h1>Second says: { props.value }</h1>;
+
+export { CompA, CompB };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import FirstComponent from './components/FirstComponent';
+// import FirstComponent from './components/FirstComponent';
+import { CompA, CompB } from './components/SecondComponent';
 
 
-const element = document.getElementById('root')
+const element = document.getElementById('root');
 ReactDOM.render(
   <div>
-    <FirstComponent value='Good Morning!' />
-    <FirstComponent value={42} />
+    <CompA value='Hi Im A!'/>
+    <CompB value='B here!' />
+
+    {/* <FirstComponent value='Good Morning!' />
+    <FirstComponent value={42} /> */}
   </div>
 , element)
