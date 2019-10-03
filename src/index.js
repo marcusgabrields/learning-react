c515f2a5bd8955db23be86e834fe7834388efef6
Gabriel <marcusgabriel@locus.software>
2019-10-03 14:51:45 -0300

    Direct communication between components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // import FirstComponent from './components/FirstComponent';
 // import CompA, { CompB } from './components/SecondComponent';
 // import MultiElements from './components/MultiElements';
-// import SilvaFamily from './components/SilvaFamily';
+import SilvaFamily from './components/SilvaFamily';
 import Family from './components/Family';
 import Member from './components/Member';
 
@@ -12,12 +12,12 @@ import Member from './components/Member';
 const element = document.getElementById('root');
 ReactDOM.render(
   <div>
-    <Family>
-      <Member name='Andre' last_name='Pereira' />
-      <Member name='Mariana' last_name='Pereira' />
+    <Family last_name='Pereira'>
+      <Member name='Andre' />
+      <Member name='Mariana' />
     </Family>
 
-    {/* <SilvaFamily /> */}
+    <SilvaFamily />
 
     {/* <MultiElements /> */}
 
