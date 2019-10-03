d5c1e7be160533b84fb9e3fd0e3082a570c0e30c
Gabriel <marcusgabriel@locus.software>
2019-10-03 14:27:47 -0300

    Relationship between components

---
+++ src/components/SilvaFamily.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Member from './Member';
+
+
+export default props =>
+  <div>
+    <Member name='Gabriel' last_name='Silva' />
+    <Member name='Daniela' last_name='Silva' />
+    <Member name='Pedro' last_name='Silva' />
+    <Member name='Ana' last_name='Silva' />
+  </div>
+
